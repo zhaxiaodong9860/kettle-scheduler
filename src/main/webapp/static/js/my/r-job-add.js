5dded741ae767e4dbdb145d6fe026038d30b79d1
xiaodongzha <382312462@qq.com>
2018-12-25 14:25:40 +0800

    Update r-job-add.js

--- src/main/webapp/static/js/my/r-job-add.js
+++ src/main/webapp/static/js/my/r-job-add.js
@@ -80,6 +80,7 @@ $("#jobPath").click(function(){
 		var index = layer.open({
 			type: 1,
 			title: '请选择作业',
+			area: ["300px", '100%'],
 			skin: 'layui-layer-rim',
 			content: '<div id="repositoryTree"></div>'
 		});
